d420e2eda73b43b05b91dd02d924d34579d5c164
Arne Van Bael <arne.van.bael@gmail.com>
2019-09-19 10:08:33 +0200

    entity type configurations with navigations

--- src/app/CQRS-Lite-Union-API.Persist.Sql/Configurations/AttendeeEntityTypeConfiguration.cs
+++ src/app/CQRS-Lite-Union-API.Persist.Sql/Configurations/AttendeeEntityTypeConfiguration.cs
@@ -11,7 +11,11 @@ namespace CQRS_Lite_Union_API.Persist.Sql.Configurations
     {
         public void Configure(EntityTypeBuilder<Attendee> builder)
         {
-            throw new NotImplementedException();
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.FirstName).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.LastName).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.Created).IsRequired();
+            builder.HasOne(x => x.Workshop).WithMany(x => x.Attendees).HasForeignKey(x => x.WorkshopId).OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
--- src/app/CQRS-Lite-Union-API.Persist.Sql/Configurations/WorkshopEntityTypeConfiguration.cs
+++ src/app/CQRS-Lite-Union-API.Persist.Sql/Configurations/WorkshopEntityTypeConfiguration.cs
@@ -1,9 +1,6 @@
 ﻿using CQRS_Lite_Union_API.Domain.Workshops;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CQRS_Lite_Union_API.Persist.Sql.Configurations
 {
@@ -11,7 +8,13 @@ namespace CQRS_Lite_Union_API.Persist.Sql.Configurations
     {
         public void Configure(EntityTypeBuilder<Workshop> builder)
         {
-            throw new NotImplementedException();
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Created).IsRequired();
+            builder.Property(x => x.Description).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.StartDate).IsRequired();
+            builder.Property(x => x.EndDate).IsRequired();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(255);
+            builder.Metadata.FindNavigation(nameof(Workshop.Attendees)).SetPropertyAccessMode(PropertyAccessMode.Field);
         }
     }
 }
